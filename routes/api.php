a06ad13d3c97fe36693587165325869100935b03
Wisdom Ebong <wisdom@ushahidi.com>
2021-05-10 10:39:05 +0100

    chore: change routes api.php to route.php and removed api/v5 group prefix

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
-$apiVersion = '5';
-$apiBase = 'api/v' . $apiVersion;
 
-$router->group([
-    'prefix' => $apiBase . '/config/data-provider',
-], function () use ($router) {
+$router->group([], function () use ($router) {
     $router->get('gmail/initialize', 'GmailController@initialize');
     $router->post('gmail/authorize', 'GmailController@authorize');
     $router->post('gmail/unauthorize', 'GmailController@unauthorize');
